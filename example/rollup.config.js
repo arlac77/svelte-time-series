879adff05090bde2d1062ee58b5662e29cf48d1f
Markus Felten <Markus.Felten@gmx.de>
2019-09-08 09:34:21 +0200

    fix: switch to rollup-plugin-dev

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,27 +1,9 @@
 import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
-import http from "http";
-import handler from "serve-handler";
 import pkg from "../package.json";
 
 const port = pkg.config.port || 5000;
-
-const development = process.env.ROLLUP_WATCH;
-
-if (development) {
-  const server = http.createServer((request, response) => {
-    return handler(request, response, {
-      public: "example/public",
-      rewrites: [{ source: "**", destination: "/index.html" }]
-    });
-  });
-
-  server.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`);
-  });
-}
 
 export default {
   input: "example/src/index.mjs",
@@ -33,12 +15,23 @@ export default {
   plugins: [
     resolve({ browser: true }),
     svelte(),
-    development && livereload("example/public"),
     dev({
       port,
       dirs: ["example/public"],
       spa: "example/public/index.html",
-      basePath: "/base"
+      basePath: "/base",
+      extend(app, modules) {
+        app.use(
+          modules.router.get("/api/data", async (ctx, next) => {
+            const lines = [];
+            for (let i = 1; i < 100; i++) {
+              lines.push({ time: i, value: i});
+            }
+            ctx.body = lines;
+            next();
+          })
+        );
+      }
     })
   ]
 };
